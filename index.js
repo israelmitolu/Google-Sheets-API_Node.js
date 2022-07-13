a8b4085a40a38f7b8d292b17c98aeeaf1cb2ecec
Israel Mitolu <israelmitolu@gmail.com>
2022-07-13 20:39:54 +0100

    Read rows from spreadsheet

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+
+const { google } = require("googleapis");
+
+// Initialise express and listen for the server on port 8080
+const app = express();
+const port = 8080;
+
+//This allows us to parse the incoming request body as JSON
+app.use(express.json());
+
+// With this, we'll listen for the server on port 8080
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// Spreadsheet ID
+const id = "12fSSjNTpDtTJ8vF4T3LRYe36WjPEfn1_sr_lSSpZJUo";
+
+//Authorise the Google client
+async function authSheets() {
+  //Function for authentication object
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "keys.json",
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+
+  //Create client instance for auth
+  const authClient = await auth.getClient();
+
+  //Instance of the Sheets API
+  const sheets = google.sheets({ version: "v4", auth: authClient });
+
+  return {
+    auth,
+    authClient,
+    sheets,
+  };
+}
+
+// Read rows from spreadsheet
+app.get("/", async (req, res) => {
+  const { sheets } = await authSheets();
+
+  const getRows = await sheets.spreadsheets.values.get({
+    spreadsheetId: id,
+    range: "Sheet1",
+  });
+
+  res.send(getRows.data);
+});
